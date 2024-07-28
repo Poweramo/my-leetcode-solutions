0930223c2f70fd4e083e5fd4452ebca67045f3a7
Poweramo <85564740+Poweramo@users.noreply.github.com>
2024-07-28 14:23:51 +0100

    Time: 66 ms (38.07%), Space: 51.1 MB (83.33%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+function mergeTwoLists(list1, list2) {
+    let head = new ListNode();
+	let trav = head;
+
+	while (list1 && list2) {
+		if (list1.val <= list2.val) {
+			trav.next = list1;
+			list1 = list1.next;
+		} else {
+			trav.next = list2;
+			list2 = list2.next;
+		}
+
+		trav = trav.next;
+	}
+
+	trav.next = list1 === null ? list2 : list1;
+	return head.next;
+};
