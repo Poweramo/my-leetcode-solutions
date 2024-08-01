1d96b9b196480cf7589388a1cca1c7aa7104c1d8
Poweramo <85564740+Poweramo@users.noreply.github.com>
2024-08-01 11:10:10 +0100

    Time: 52 ms (96.19%), Space: 52.9 MB (63.12%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -14,16 +14,14 @@
 function hasCycle(head) {
     let slow = head;
 	let fast = head;
-
-	slow = slow?.next;
-	fast = fast?.next?.next;
 	
-	while (slow !== fast) {
-		slow = slow?.next;
-		fast = fast?.next?.next;
+	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
 		
+        if (slow === fast) return true
 	}
  
-	if (!slow && !fast) return false
-    if (slow === fast) return true
+	return false
+    
 }
