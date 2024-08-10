93b207c80861ee3f235e61263f94c53a6cd03ca7
Poweramo <85564740+Poweramo@users.noreply.github.com>
2024-08-10 15:22:11 +0100

    0001-TWO-SUM Stats: Time: 56 ms (81.02%), Space: 51.5 MB (23.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,11 +5,12 @@
  */
 
 function twoSum(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-		for (let j = 0; j < nums.length; j++) {
-			if (nums[j] + nums[i] === target && i !== j) {
-				return [i, j];
-			}
-		}
+	const numsMap = {};
+
+	for (let i = 0; i < nums.length; i++) {
+		const num = nums[i];
+		const diff = target - num;
+		if (diff in numsMap) return [i, numsMap[diff]];
+		numsMap[num] = i;
 	}
-};
+}
