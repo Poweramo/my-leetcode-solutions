c41748c9526e82f4452fa0cc9b232877e2242ef3
Poweramo <85564740+Poweramo@users.noreply.github.com>
2024-08-10 13:33:17 +0100

    0217-CONTAINS-DUPLICATE Stats: Time: 82 ms (49.04%), Space: 66.7 MB (7.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,18 +4,13 @@
  */
 
 function containsDuplicate(nums) {
-	const map = new Map();
-	function hash(input) {
-		return input * 2;
-	}
+	const frequencyMap = new Map();
 
 	for (let i = 0; i < nums.length; i++) {
 		const num = nums[i];
 
-		if (map.has(hash(num))) {
-			return true;
-		}
-		map.set(hash(num), num);
+		if (frequencyMap.has(num)) return true;
+		frequencyMap.set(num, "exist");
 	}
 
 	return false;
