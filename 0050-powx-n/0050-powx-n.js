c2862cc9a12584c2a603f3df8b865107d63fcef9
Poweramo <85564740+Poweramo@users.noreply.github.com>
2024-08-31 11:28:27 +0000

    0050-POWX-N Stats: Time: 52 ms (62.56%), Space: 49.1 MB (38.94%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -3,6 +3,19 @@
  * @param {number} n
  * @return {number}
  */
+
 function myPow(x, n) {
-    return x ** n
-};
+    function calculateRes(number, exponent) {
+        if (number === 0) return 0;
+        if (exponent === 0) return 1;
+
+        let res = calculateRes(number, Math.floor(exponent / 2));
+        res = res * res;
+
+        if (exponent % 2 === 1) return res * number;
+        return res;
+    }
+
+    let ans = calculateRes(x, Math.abs(n));
+    return n >= 0 ? ans : 1 / ans;
+}
